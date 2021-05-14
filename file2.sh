f01a3bef771470cd050a911ea2c4d9ade6eeaf2e
root <root@ip-172-31-47-87.us-east-2.compute.internal>
2021-05-14 09:12:28 +0000

    file2.sh

--- file2.sh
+++ file2.sh
@@ -1,7 +1,7 @@
 This is a file2 of 1st line
 This is a file2 of 2nd line
 This is a file2 of 3rd line
-mani01branch
+mani01branch1
 This is a file2 of 4th line
 This is a file2 of 6th line
 This is a file2 of 7th line
